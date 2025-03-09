6e72f34d230d7884ba0025c0f7153d95ac0bea44
himandeccaleap <himan.deccaleap@gmail.com>
2025-03-09 19:44:53 +0530

    first commit

--- src/pages/home3/index.jsx
+++ src/pages/home3/index.jsx
@@ -5,21 +5,12 @@ import AfterBefore from "../../components/After-Before";
 import Blogs2 from "../../components/Blogs2";
 import Intro3 from "../../components/Intro3";
 import Numbers from "../../components/Numbers";
-<<<<<<< HEAD
-import Services1 from "../../components/Services3";
-import Team1 from "../../components/Team1";
-import Testimonials1 from "../../components/Testimonials1";
-import VideoWithTestimonials from "../../components/Video-with-testimonials"; 
-import Works3 from "../../components/Work-Four-Column";
-import Video from "../../components/Project-Video";
-=======
 import Services3 from "../../components/Services3";
 import Team1 from "../../components/Team1";
 import Testimonials1 from "../../components/Testimonials1";
 import VideoWithTestimonials from "../../components/Video-with-testimonials"; 
 import Works4 from "../../components/Work-Four-Column";
 
->>>>>>> fad9ec5b61506183895c2a7f5fda1da59fa7fd5d
 import SkillsCircle2 from "../../components/Skills-Circle2";
 import Brands2 from "../../components/Brands";
 import LightLayout from "../../layouts/light";
@@ -31,20 +22,11 @@ const Home3 = () => {
     <LightLayout>
       <Intro3 />
       <AboutUs6/>
-<<<<<<< HEAD
-      <Services1/>
-      <Numbers />
-      {/* <Team1 />
-      <AfterBefore /> */}
-      {/* <Video /> */}
-      <Works3 />
-=======
       <Services3/>
       {/* <Numbers /> */}
       {/* <Team1 /> */}
       {/* <AfterBefore /> */}
       <Works4 />
->>>>>>> fad9ec5b61506183895c2a7f5fda1da59fa7fd5d
       <Testimonials1 />
       <Blogs2 />
     </LightLayout>
