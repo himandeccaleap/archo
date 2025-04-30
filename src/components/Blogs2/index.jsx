59e804def65a86a6331f0442614fb7b211c1eb28
himandeccaleap <himan.deccaleap@gmail.com>
2025-04-30 19:53:05 +0530

    first commit

--- src/components/Blogs2/index.jsx
+++ src/components/Blogs2/index.jsx
@@ -22,7 +22,7 @@ const Blogs2 = () => {
 
         <div className="row">
           {Blog1Data.slice(0, 2).map((blog, index) => {
-            const blogLink = typeof blog.Link === "string" ? blog.Link : "/blogs"; // Fallback
+            const blogLink = typeof blog.Link === "string" ? blog.Link : "/blogs";
 
             return (
               <div className="col-lg-6" key={blog.id}>
@@ -36,17 +36,24 @@ const Blogs2 = () => {
                     </div>
                   </div>
                   <div className="cont">
-                  <div className="info">
-                    <Link href={blogLink}><span>{blog.by}</span></Link>
-                    <Link href={blogLink}><span>{blog.date}</span></Link>
-                  </div>
+                    <div className="info">
+                      <Link href={blogLink}>
+                        <span>{blog.by}</span>
+                      </Link>
+                      {" | "}
+                      <Link href={blogLink}>
+                        <span>{blog.date}</span>
+                      </Link>
+                    </div>
 
                     <h5 className="playfont">
-                      <Link href={blogLink}><span>{blog.title}</span></Link>
+                      <Link href={blogLink}>
+                        <span>{blog.title}</span>
+                      </Link>
                     </h5>
 
-                    <Link href={blogLink}>
-                      <span className="more custom-font">Read More</span>
+                    <Link href={blogLink} className="more">
+                      <span className="custom-font">Read More</span>
                     </Link>
                   </div>
                 </div>
@@ -55,7 +62,6 @@ const Blogs2 = () => {
           })}
         </div>
 
-        {/* Blog Grid Button */}
         <div className="row mt-4">
           <div className="col-12 text-center">
             <Link href="/blog-grid">
